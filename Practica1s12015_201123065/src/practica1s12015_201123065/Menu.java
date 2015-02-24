c8422c0c07f06a785a364479415dd7e94a05b9da
201123065 <heyymarcos@gmail.com>
2015-02-23 23:14:02 -0600

    NuevaCarga

--- Practica1s12015_201123065/src/practica1s12015_201123065/Menu.java
+++ Practica1s12015_201123065/src/practica1s12015_201123065/Menu.java
@@ -417,7 +417,6 @@ public class Menu extends javax.swing.JFrame {
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         catalogo cat = new catalogo();
         cat.setVisible(true);
-        
     }//GEN-LAST:event_jButton4ActionPerformed
 
     /**
